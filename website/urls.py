7a1e11edfbab9cf6d83bcdac5ded66b3a8e59b45
Seejan Sunar <czansunar0098@gmail.com>
2022-09-09 23:27:44 +0545

    rendered first view

--- website/urls.py
+++ website/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from website import views
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.index),
+    
 ]
